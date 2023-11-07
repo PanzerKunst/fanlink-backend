c0501f3bdef7bc2e79e782a0d2f4d1d830f3dc55
Posidonius <cbramdit@gmail.com>
2023-11-07 09:36:36 +0100

    WIP

--- src/config.ts
+++ src/config.ts
@@ -1,11 +1,9 @@
 type Config = {
   readonly PORT: number;
-  readonly PG_USERNAME: string;
-  readonly PG_PWD: string;
+  readonly DATABASE_URL: string;
 }
 
 export const config: Config = {
   PORT: Number(process.env.PORT!),
-  PG_USERNAME: process.env.PG_USERNAME!,
-  PG_PWD: process.env.PG_PWD!
+  DATABASE_URL: process.env.DATABASE_URL!
 }
