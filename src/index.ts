a62605bafa5d8cbca5080bbd9eaa204467970c92
Posidonius <cbramdit@gmail.com>
2023-11-08 08:38:57 +0100

    WIP

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,26 @@
 import * as dotenv from "dotenv"
 dotenv.config()
 
-import express, { NextFunction, Request, Response } from "express"
+import express, { Request, Response } from "express"
 import cors from "cors"
 import { config } from "./config"
 import { SpotifyArtist } from "./Models/Spotify/SpotifyArtist"
 import { migrateDb } from "./DB/DB"
 import { insertArtists } from "./DB/Schema/Artists"
+
+// Catch unhandled promise rejections
+process.on("unhandledRejection", (reason, promise) => {
+  console.error("Unhandled Rejection at:", promise, "reason:", reason)
+  // Your error logging or cleanup code here
+})
+
+// Catch uncaught exceptions
+process.on("uncaughtException", (error) => {
+  console.error("Uncaught Exception:", error)
+  // Your error logging or cleanup code here
+  // It is not safe to continue running the server after an uncaught exception.
+  // You should consider shutting down the process gracefully.
+})
 
 const app = express()
 const port = config.PORT
@@ -16,6 +30,9 @@ app.use(express.json())
 // Configure CORS middleware options
 const corsOptions: cors.CorsOptions = {
   origin: (origin, callback) => {
+    // Allow requests with no origin
+    if (!origin) return callback(null, true)
+
     if (config.FRONTEND_URL !== origin) {
       const msg = "The CORS policy for this site does not allow access from the specified Origin."
       return callback(new Error(msg), false)
@@ -38,12 +55,6 @@ app.post("/artists", async (req: Request, res: Response) => {
   const insertedArtists = await insertArtists(spotifyArtists)
 
   res.status(200).json(insertedArtists)
-})
-
-// Error handling middleware
-app.use((error: any, req: Request, res: Response) => { // eslint-disable-line @typescript-eslint/no-explicit-any
-  console.error(error.stack)
-  res.status(500).send("Something broke!")
 })
 
 app.listen(port, () => {
