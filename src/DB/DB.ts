3c5d07b58e594a5462fe51b9d6cf68c3bf1fb829
Posidonius <cbramdit@gmail.com>
2023-12-08 13:39:42 +0100

    WIP

--- src/DB/DB.ts
+++ src/DB/DB.ts
@@ -22,7 +22,7 @@ const connectionOptions = config.IS_PROD ? {
 }
 
 export const pgSql = postgres(config.DATABASE_URL, connectionOptions)
-export const db = drizzle(pgSql,/* TODO  { logger: true } */)
+export const db = drizzle(pgSql, { logger: true })
 
 export async function migrateDb(): Promise<void> {
   await migrateTableMusicGenres()
