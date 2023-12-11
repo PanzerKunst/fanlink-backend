97476f2b5bda5ffb7a3957e1a375e17f6a9b2599
Posidonius <cbramdit@gmail.com>
2023-12-11 10:30:47 +0100

    WIP

--- src/index.ts
+++ src/index.ts
@@ -34,7 +34,7 @@ app.use(express.raw({ type: "text/plain", limit: payloadLimit }))
 app.use(express.urlencoded({ limit: payloadLimit, extended: true }))
 
 // TODO: remove
-console.log("Environment variables:", config)
+console.log("Environment variables :", config)
 
 // Configure CORS middleware options
 const corsOptions: cors.CorsOptions = {
