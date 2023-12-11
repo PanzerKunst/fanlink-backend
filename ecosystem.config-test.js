b864c2f2bd549135146f6e5441a3663874c404a7
Posidonius <cbramdit@gmail.com>
2023-12-11 10:02:15 +0100

    WIP

--- ecosystem.config-test.js
+++ ecosystem.config-test.js
@@ -5,7 +5,10 @@ module.exports = {
     args: "run start",
     time: true,
     env: {
-      NODE_ENV: "production"
+      NODE_ENV: "production",
+      PORT: "3000",
+      DATABASE_URL: "postgresql://postgres:6gW^tm@4MC1T@localhost:5432/backstage_pass_test",
+      FRONTEND_URL: "https://fanlink-murex.vercel.app"
     },
     out_file: "/home/panzerkunst/fanlink-backend-test/log/app.log",
     error_file: "/home/panzerkunst/fanlink-backend-test/log/error.log",
