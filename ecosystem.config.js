e5d57cd7393f82d4290a1c0f1ce075bfb3519520
Posidonius <cbramdit@gmail.com>
2024-01-26 16:28:16 +0100

    WIP

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,9 +3,6 @@ module.exports = {
     name: "BackstagePassBackendTest",
     script: "npm",
     args: "run start",
-    env: {
-      NODE_ENV: "production"
-    },
     out_file: "/home/panzerkunst/fanlink-backend-test/logs/out.log",
     error_file: "/home/panzerkunst/fanlink-backend-test/logs/error.log",
     merge_logs: true,
